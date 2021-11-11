34b46a1ca7bb959416f7f3f4c15bb679959aede6
zhanggangbin <zhanggangbin@tcsoft.info>
2021-11-11 17:28:50 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueRouter from './router'
 
-createApp(App).mount('#app')
+createApp(VueRouter, App).mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import VueRouter from 'vue-router'
+
+const app = createApp({})
+
+app.use(VueRouter)
+
+export default VueRouter
